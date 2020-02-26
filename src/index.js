a1e8d4bceb73fdb5d6c0a09f52259167b0b99578
vadzim-su <vadimmm0991@gmail.com>
2020-02-26 22:53:29 +0300

    feat: add correct solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array !== undefined && array.length > 0) {
+    array.sort(function (a, b) { return a - b; })
+    return array[0];
+  }
+  else {
+    return 0
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array !== undefined && array.length > 0) {
+    array.sort(function (a, b) { return a - b; })
+    array = array.reverse('')
+    return array[0];
+  }
+  else {
+    return 0
+  }
+
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array !== undefined && array.length > 0) {
+    let result = array.reduce((sum, current) => sum + current, 0);
+    return result / array.length
+  }
+  else {
+    return 0
+  }
+
 }
